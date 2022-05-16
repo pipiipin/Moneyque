23c517c44a4359a5561876ec4b3afa37e26c7650
iceing11 <ice_iceing@hotmail.com>
2022-05-17 04:34:06 +0700

    [UPDATE] listing update

--- moneyque/lib/listing.dart
+++ moneyque/lib/listing.dart
@@ -10,8 +10,44 @@ class Listing extends StatefulWidget {
 class _ListingState extends State<Listing> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Listing Page'),
+    return new MaterialApp(
+      title: 'Testing',
+      home: new SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        print(1);
+                      },
+                      child: Icon(
+                        Icons.swap_horiz_rounded,
+                        size: 40,
+                      ),
+                    ),
+                    Text("Buy"),
+                    GestureDetector(
+                      onTap: () {
+                        print(2);
+                      },
+                      child: Icon(
+                        Icons.find_in_page_outlined,
+                        size: 40,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: Colors.white,
+        ),
+      ),
     );
   }
 }
