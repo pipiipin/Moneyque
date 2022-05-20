1247ce2ce3fa145c105ffe94126612e98a08e6da
iceing11 <ice_iceing@hotmail.com>
2022-05-21 03:26:35 +0700

    [UPDATE] listing page frontend

--- moneyque/lib/listing.dart
+++ moneyque/lib/listing.dart
@@ -10,14 +10,58 @@ class Listing extends StatefulWidget {
 class _ListingState extends State<Listing> {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    var tags = [
+      {'name': 'Poverty'},
+      {'name': 'Education'},
+      {'name': 'Gender Equality'},
+      {'name': 'Life On Land'},
+      {'name': 'Life Below Water'},
+      {'name': 'Peace'},
+    ];
+
+    var posts = [
+      {
+        'name': 'Project For Good',
+        'author': 'Don Quixote',
+        'desc':
+            'A placeholder description. It needs to be long so I can test the lines.',
+        'tag': 'Robot',
+        'avatar': ''
+      },
+      {
+        'name': 'Project For Good',
+        'author': 'Don Quixote',
+        'desc':
+            'A placeholder description. It needs to be long so I can test the lines.',
+        'tag': 'Robot',
+        'avatar': ''
+      },
+      {
+        'name': 'Project For Good',
+        'author': 'Don Quixote',
+        'desc':
+            'A placeholder description. It needs to be long so I can test the lines.',
+        'tag': 'Robot',
+        'avatar': ''
+      },
+      {
+        'name': 'Project For Good',
+        'author': 'Don Quixote',
+        'desc':
+            'A placeholder description. It needs to be long so I can test the lines.',
+        'tag': 'Robot',
+        'avatar': ''
+      },
+    ];
+
+    return MaterialApp(
       title: 'Testing',
-      home: new SafeArea(
+      home: SafeArea(
         child: Scaffold(
           body: Column(
             children: [
               Padding(
-                padding: EdgeInsets.all(25),
+                padding: EdgeInsets.fromLTRB(25, 25, 25, 15),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -30,7 +74,16 @@ class _ListingState extends State<Listing> {
                         size: 40,
                       ),
                     ),
-                    Text("Buy"),
+                    DefaultTextStyle(
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      child: Text('Buy'),
+                    ),
                     GestureDetector(
                       onTap: () {
                         print(2);
@@ -43,6 +96,29 @@ class _ListingState extends State<Listing> {
                   ],
                 ),
               ),
+              Divider(
+                height: 0,
+                thickness: 2,
+                indent: 30,
+                endIndent: 30,
+                color: Color.fromRGBO(225, 225, 225, 1),
+              ),
+              Padding(
+                padding: EdgeInsets.all(7),
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: 8.0, // gap between adjacent chips
+                  runSpacing: -8.0, // gap between lines
+                  children: tags
+                      .map(
+                        (e) => Chip(
+                          label: Text(e['name']!),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              Column(),
             ],
           ),
           backgroundColor: Colors.white,
